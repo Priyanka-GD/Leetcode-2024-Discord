575d5baa32c7fb12f57a568a1a95d75c9d0732a7
“PriyankaGD” <priyanka.dastidar@gmail.com>
2024-09-05 16:32:12 -0700

    New Repo

---
+++ src/main/java/Sep6/MedianDataStreamOne.java
@@ -0,0 +1,44 @@
+package Sep6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MedianDataStreamOne {
+        List<Integer> listOfIntegers;
+        int size;
+
+        public MedianDataStreamOne() {
+            listOfIntegers = new ArrayList<>();
+            size = 0;
+        }
+
+        public void addNum(int num) {
+            int findIdxToInsert = binarySearch(num);
+            listOfIntegers.add(findIdxToInsert, num);
+            size++;
+        }
+
+        public double findMedian() {
+            if (size % 2 == 0) {
+                return (listOfIntegers.get(size / 2)
+                        + listOfIntegers.get(size / 2 - 1)) / 2.0;
+            } else {
+                return listOfIntegers.get(size / 2);
+            }
+        }
+
+        private int binarySearch(int num) {
+            int low = 0;
+            int high = listOfIntegers.size();
+
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (listOfIntegers.get(mid) >= num) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+}
