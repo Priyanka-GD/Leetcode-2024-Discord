43958f678668dd91171d9cc4f233924e816cf242
“PriyankaGD” <priyanka.dastidar@gmail.com>
2024-09-18 16:16:14 -0700

    Multithread synchronized

--- src/main/java/multithreading/ThreadTester.java
+++ src/main/java/multithreading/ThreadTester.java
@@ -31,10 +31,11 @@ public class ThreadTester {
 
         // Block of code inside synchronized is a critical section
 
-        Stack stack = new Stack(5);
+        //Stack stack = new Stack(5);
         //implementation of run method of runnable interface
 
-        new Thread(() ->
+       /*
+       new Thread(() ->
         {
             int counter = 0;
             while (++counter < 10) {
@@ -50,7 +51,74 @@ public class ThreadTester {
             }
         }, "Popper").start();
 
+        Thread thread3 = new Thread(() ->
+        {
+            try {
+                Thread.sleep(1);
+
+                for (int i = 10000; i > 0; i--) {
+                    //System.out.println("Pushed : " + Thread.currentThread() + " " + stack.push(100));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "States");
+        thread3.start();
+        while (true) {
+            Thread.State state = thread3.getState();
+            System.out.println(state);
+            if (state == Thread.State.TERMINATED)
+                break;
+        }
+
+        Thread thread = new Thread(() ->
+        {
+            System.out.println(Thread.currentThread());
+        }, "Our Thread");
+        thread.start();
+        try {
+            thread.join();// Child threads completes first, blocked main from executing further, Sequential Execution
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        String lock1 = "Lock1";
+        String lock2 = "Lock2";
+        Thread thread1 = new Thread(() ->
+        {
+            synchronized (lock1) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("Lock Acquired by " + Thread.currentThread());
+                }
+            }
+        }, "thread1");
+        thread1.start();
+        Thread thread2 = new Thread(() ->
+        {
+            synchronized (lock2) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    System.out.println("Lock Acquired by " + Thread.currentThread());
+                }
+            }
+        }, "thread2");
+        thread2.start();
+        //Deadlocks - One thread waiting for another thread to relinquish a lock.
+        //Wait indefinitely, program went into deadlock situation
+        //To avoid deadlock, locks should be acquired in same sequence.
 
         System.out.println("Main is Exiting");
     }
 }
+/*
+ * Deadlocks - One thread waiting for another thread to relinquish a lock.
+ *  */
